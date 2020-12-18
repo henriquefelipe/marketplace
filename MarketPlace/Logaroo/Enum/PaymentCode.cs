038a2918e0b4fde8f2e8a9f3c8321ba89f32e45e
Henrique Felipe <henrique.felipe85@gmail.com>
2020-12-18 12:39:41 -0300

    Colocando os novos códigos do logaroo;

--- MarketPlace/Logaroo/Enum/PaymentCode.cs
+++ MarketPlace/Logaroo/Enum/PaymentCode.cs
@@ -16,6 +16,12 @@ namespace Logaroo.Enum
         public const string CartaoDeCreditoPagamentoOnline = "5";
         public const string CartaoDeDebitoPagamentoOnline = "6";
         public const string VoucherLojista = "7";
-        public const string VoucherRestaurante = "8";       
+        public const string VoucherRestaurante = "8";
+        public const string LogarooCartaoCredito = "9";
+        public const string LogarooCartaoDebito = "10";
+        public const string MercadoPago = "11";
+        public const string PayPal = "12";
+        public const string PicPay = "13";
+        public const string Pix = "14";
     }
 }
