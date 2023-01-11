c2b15ad61932623555664ffba6fab41723af55ee
Rafael Viktor <rafaellviktor@gmail.com>
2023-01-11 09:41:10 -0300

    Enums pagamento

---
+++ MarketPlace/QueroDelivery/Enum/paymentMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueroDelivery.Enum
+{
+    public class PaymentMethods
+    {
+        public const string CREDIT = "CREDIT";
+        public const string DEBIT = "DEBIT";
+        public const string DIGITAL_WALLET = "DIGITAL_WALLET";
+        public const string CASH = "CASH";
+        public const string PIX = "PIX";
+    }
+}
---
+++ MarketPlace/QueroDelivery/Enum/paymentTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueroDelivery.Enum
+{
+    public class PaymentTypes
+    {
+        public const string PREPAID = "PREPAID";
+        public const string PENDING = "PENDING";
+    }
+}
