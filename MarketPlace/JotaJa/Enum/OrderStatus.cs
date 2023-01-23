7ffcd70f34379f17399ebf74fbd2a984e222ce74
Rafael Viktor <rafaellviktor@gmail.com>
2023-01-23 10:35:23 -0300

    Enum status  de pedidos

---
+++ MarketPlace/JotaJa/Enum/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JotaJa.Enum
+{
+    public class OrderStatus
+    {
+        public const string NEW_ORDER = "NEW_ORDER"; //A Aceitar
+        public const string CONFIRMED = "CONFIRMED"; // Em Produção
+        public const string PICKED_UP = "PICKED_UP"; // Saiu para Entrega
+        public const string CANCELED = "CANCELED"; // Deletado
+        public const string FULFILLED = "FULFILLED"; // Finalizado
+        public const string PENDING = "PENDING"; // Pagamento Pendente
+        public const string PREPARED = "PREPARED"; // Preparado (Para retirada)
+    }
+}
