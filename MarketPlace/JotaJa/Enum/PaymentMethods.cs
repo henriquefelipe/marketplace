1e1bd72eb507a77db85846e5e98df3b00b5f6080
Rafael Viktor <rafaellviktor@gmail.com>
2023-01-26 16:22:21 -0300

    criado classe enum das formas de pagamento principais mais utilizados no JotaJa

---
+++ MarketPlace/JotaJa/Enum/PaymentMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JotaJa.Enum
+{
+    public class PaymentMethods
+    {
+        public const string Dinheiro = "Dinheiro";
+
+        // Cartões
+        public const string Debito = "Débito";
+        public const string Credito = "Crédito";
+        public const string CartaodeCredito = "Cartão de Crédito";
+        public const string HiperCard = "Hiper Card";
+        public const string Visa = "Visa";
+        public const string VisaElectron = "Visa Electron";
+        public const string Maestro = "Maestro";
+        public const string Mastercard = "Mastercard";
+        public const string AmericanExpress = "American Express";
+        public const string EloDebito = "Elo Débito";
+        public const string EloCredito = "Elo Crédito";
+
+        //Vales Refeição/Alimentação
+        public const string TicketRestaurante = "Ticket Restaurante";
+        public const string TicketAlimentacao = "Ticket Alimentação";
+        public const string BenVisaValeRefeicao = "Ben Visa Vale Refeição";
+        public const string VRRefeicao = "VR Refeição";
+        public const string VRAlimentacao = "VR Alimentação";
+        public const string SodexoRefeicao = "Sodexo Refeição";
+        public const string AleloRefeicao = "Alelo Refeição";
+        public const string AleloAlimentacao = "Alelo Alimentação";
+        public const string SodexoAlimentacao = "Sodexo Alimentação";
+    }
+}
