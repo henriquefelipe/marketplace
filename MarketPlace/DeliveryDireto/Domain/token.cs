25b4a91038a7e578b88779410382c675a4bc185a
Rafael Viktor <rafaellviktor@gmail.com>
2023-03-01 19:14:13 -0300

    classe token nova API Delivery Direto

---
+++ MarketPlace/DeliveryDireto/Domain/token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryDireto.Domain
+{
+    public class token
+    {
+        public string token_type { get; set; }
+        public decimal expires_in { get; set; }
+        public string access_token { get; set; }
+        public string refresh_token { get; set; }
+    }
+}
