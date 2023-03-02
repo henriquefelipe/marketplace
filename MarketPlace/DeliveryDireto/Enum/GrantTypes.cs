bdec46c495fc18162a9e287a6c93dff2a089a283
Rafael Viktor <rafaellviktor@gmail.com>
2023-03-01 22:00:02 -0300

    GrantTypes Enum - Delivery Direto Nova API

---
+++ MarketPlace/DeliveryDireto/Enum/GrantTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryDireto.Enum
+{
+    public class GrantTypes
+    {
+        public const string PASSWORD = "password";
+        public const string REFRESH_TOKEN = "refresh_token";
+    }
+}
