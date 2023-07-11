e6cb559f78fab0a869cd676c7b2af957608d74be
Rafael Viktor <rafael@izzyway.com.br>
2023-07-11 17:12:16 -0300

    Marketplace MultiPedidos - Adicionado campos JSON referentes a Pizzas

---
+++ MarketPlace/MultiPedido/Domain/additionalToppings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiPedido.Domain
+{
+    public class additionalToppings
+    {
+        public int? id {  get; set; }
+        public string name { get; set; }
+        public decimal? price { get; set; }
+        public decimal? available { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
+        public int? restaurant_id { get; set; }
+        public int? toppings_category_id { get; set; }
+        public string external_id { get; set; }
+        public decimal? cashier_id { get; set; }
+    }
+}
---
+++ MarketPlace/MultiPedido/Domain/crust.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiPedido.Domain
+{
+    public class crust
+    {
+        public int? menu_item_id { get; set; }
+        public int? id { get; set; }
+        public string name { get; set; }
+        public decimal? price { get; set; }
+        public string external_id { get; set; }
+        public int? restaurant_id { get; set; }
+        public decimal? cashier_id { get; set; }
+    }
+}
