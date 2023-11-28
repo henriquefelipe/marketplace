556298b1f01fd1b24c8075d4b59399e85b011ca5
Robson Soares <robsondssd@gmail.com>
2023-11-27 21:46:06 -0300

    bug #hotfix(integrador  ipos): corrige campo ncm em sizes

--- MarketPlace/MultiPedido/Domain/size.cs
+++ MarketPlace/MultiPedido/Domain/size.cs
@@ -20,11 +20,11 @@ namespace MultiPedido.Domain
         public string external_id { get; set; }
         public string type { get; set; }
         public crust crust { get; set; }
-        public double menu_price { get; set; }
-        public double item_sub_total { get; set; }
+        public decimal menu_price { get; set; }
+        public decimal item_sub_total { get; set; }
         public bool is_combo { get; set; }
         public string menu_name { get; set; }
-        public int ncm { get; set; }
+        public string ncm { get; set; }
         public int id { get; set; }
         public List<dough> dough { get; set; }
     }
